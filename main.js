8c244c0fce351d465421ca3e48fe57a46ed276f3
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-06-25 19:28:37 +0200

    Working, need to add README

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import "./scss/main.scss";
-import { setupCounter } from './counter.js'
 import initMap from './lib/map.js'
 document.querySelector('#app').innerHTML = `
 <div id="content">
     <div id="map">
-    
     </div>
     <div id="form-layer">
         <form id="form">
             <label for="place-name">Place name</label>
             <input type="text" id="place-name" placeholder="Type a name..." />
-            <textarea id="place-details" placeholder="Type a description"></textarea>
+            <label for="place-details">Description</label>
+            <textarea id="place-details" placeholder="Type a description" rows="6"></textarea>
             <label for="coords-output">Coordinates</label>
-            <textarea id="coords-output" rows="10" disabled></textarea>
+            <textarea id="coords-output" rows="4" disabled></textarea>
             <button id="save-button">Save</button>
             <button id="close-button">Close</button>
        </form>
     </div>
 </div>
+<footer>
+<p>js-responsive-map | Author: Maciej Janta-Lipiński INF II NS</p>
+</footer>
 `
 
-initMap();
+initMap(54.4533521,17.0419606,15);
